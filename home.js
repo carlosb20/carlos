e1671e65e3c9c22434623c83368bc96e6037cd15
carlosb20 <carlosalbertosnb20@gmail.com>
2025-12-30 19:57:31 -0300

    atualizado

--- home.js
+++
@@ -1,6 +0,0 @@
-const botao = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu2");
-
-botao.addEventListener("click", () => {
-    menu.classList.toggle("ativo");
-});
---
+++ home01.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    console.log('ok')
+});
