8a0c502da8160117a401689d713702aa1e7d031b
carlosb20 <carlosalbertosnb20@gmail.com>
2025-12-31 01:09:52 -0300

    atualizado

--- home01.js
+++ home01.js
@@ -1,7 +1,26 @@
 const btn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
+const menu_a = [...document.querySelectorAll('.menu > ul > li > a')]
+
+
+
+
+var material_json = NaN
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
-    console.log('ok')
+    
 });
+
+
+menu_a.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        
+        window.location.href = `home02.html?nome=${e.textContent}`
+        
+        
+    })
+    
+})
+ 
+
--- home02.js
+++ home02.js
@@ -1,1 +1,14 @@
 
+const val = document.querySelector('.mck')
+
+console.log(val)
+
+
+const params = new URLSearchParams(window.location.search);
+
+const nome = params.get('nome');
+
+val.innerHTML = `${nome}`
+
+console.log(nome);  // Joao
+  
