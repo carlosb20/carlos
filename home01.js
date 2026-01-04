4784be788581708c4cb12011c20d5bce4f02eaa5
carlosb20 <carlosalbertosnb20@gmail.com>
2026-01-03 23:52:37 -0300

    atualizado

--- home01.js
+++ home01.js
@@ -23,45 +23,51 @@ menu_a.forEach((e)=>{
 })
  
 // ----------------- container Muck ---------------------
-const muck = 'Guindaste';
-const divcont_muck = document.createElement('div');
+const muck = 'Guindaste'
+const divcont_muck = document.createElement('div')
 
-divcont_muck.style.display = 'block';
+//divcont_muck.style.display = '100%'
 
-const h1_text = document.createElement('h1');
-h1_text.innerHTML = 'Muck';
-h1_text.style.padding = '5px';
-divcont_muck.appendChild(h1_text);
+const h1_text = document.createElement('h1')
+h1_text.innerHTML = 'Muck'
+h1_text.style.padding = '5px'
+divcont_muck.appendChild(h1_text) 
 
-const contdiv = document.createElement('div');
-contdiv.className = 'contdiv';
-contdiv.style.display = 'flex';
-contdiv.style.flexWrap = 'wrap';
+const contdiv = document.createElement('div')
+contdiv.setAttribute('class','contdiv')
+contdiv.style.display = 'flex'
+contdiv.style.width = '5000px'
 
-divcont_muck.appendChild(contdiv);
 
-lista_material[muck].forEach((e) => {
+divcont_muck.appendChild(contdiv)
 
-    console.log(e.foto, 'ok');
+lista_material[muck].forEach((e)=>{
 
-    const cont_muck = document.createElement('div');
-    const img_ = document.createElement('img');
-
-    cont_muck.style.width = '200px';
-    cont_muck.style.height = '200px';
-    cont_muck.style.boxShadow = '0px 0px 10px -1px black';
-    cont_muck.style.margin = '5px';
-
-    img_.src = e.foto;
-    img_.style.width = '100%';
-    img_.style.height = '100%';
+    console.log(e.foto , 'ok')
+    const cont_muck = document.createElement('div')
+    const img_ = document.createElement('img')
+    
+    //cont_muck.style.backgroundColor = 'red'
+    cont_muck.style.width = '200px'
+    cont_muck.style.height = '200px'
+    cont_muck.style.boxShadow = '0px 0px 10px -1px black'
+    cont_muck.style.margin = '5px'
+    
+    img_.src = `${e.foto}`
+    img_.style.width = '200px';
+    img_.style.height = '200px';
     img_.style.objectFit = 'cover';
+    
+    
+    cont_muck.appendChild(img_)
+    contdiv.appendChild(cont_muck)
+    
 
-    cont_muck.appendChild(img_);
-    contdiv.appendChild(cont_muck);
-});
+})
 
-main.appendChild(divcont_muck);
+
+main.appendChild(divcont_muck)
+
 
 // ------------------------------------------------
 
